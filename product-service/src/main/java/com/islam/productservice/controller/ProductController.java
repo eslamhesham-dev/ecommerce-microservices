be98abd09176c1d5228d146723c48a11ea4b7b6d
eslamhesham-dev <eslam_sharaf@rayacorp.com>
2026-06-24 12:36:27 +0300

    Set up Spring Cloud microservices infrastructure

---
+++ product-service/src/main/java/com/islam/productservice/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.islam.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Product service is working";
+    }
+}
